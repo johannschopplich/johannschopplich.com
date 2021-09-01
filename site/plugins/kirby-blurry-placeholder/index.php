111defba060f0df1e819eb103cbf8a07c5b21363
Johann Schopplich <mail@johannschopplich.com>
2021-09-01 13:36:44 +0200

    chore: upgrade dependencies

--- site/plugins/kirby-blurry-placeholder/index.php
+++ site/plugins/kirby-blurry-placeholder/index.php
@@ -1,25 +1,16 @@
 <?php
 
-@include_once __DIR__ . '/vendor/autoload.php';
+load([
+    'KirbyExtended\\BlurryPlaceholder' => 'classes/KirbyExtended/BlurryPlaceholder.php'
+], __DIR__);
 
-use Kirby\Cms\App as Kirby;
-use KirbyExtended\BlurryPlaceholder;
-
-Kirby::plugin('kirby-extended/blurry-placeholder', [
-    'options' => [
-        'pixel-target' => 60,
-        'srcset' => [
-            'enable' => false,
-            'preset' => null,
-            'sizes' => 'auto'
-        ]
-    ],
+\Kirby\Cms\App::plugin('kirby-extended/blurry-placeholder', [
     'fileMethods' => [
         'placeholder' => function () {
-            return BlurryPlaceholder::image($this);
+            return \KirbyExtended\BlurryPlaceholder::image($this);
         },
         'placeholderUri' => function () {
-            return BlurryPlaceholder::uri($this);
+            return \KirbyExtended\BlurryPlaceholder::uri($this);
         }
     ],
     'tags' => [
