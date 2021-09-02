f24fb8765f3bcb9b7db425956797ec4e5f897898
Johann Schopplich <mail@johannschopplich.com>
2021-09-02 23:27:08 +0200

    refactor: image snippet

--- site/snippets/image.php
+++ site/snippets/image.php
@@ -2,15 +2,16 @@
 
 use Kirby\Toolkit\Html;
 
-if (isset($image) && $image !== null) {
-  echo Html::img($image->placeholderUri(), [
-    'class' => $class ?? null,
-    'loading' => 'lazy',
-    'data-srcset' => $image->srcset(),
-    'data-sizes' => 'auto',
-    'data-zoomable' => $zoomable ?? null,
-    'width' => $image->width(),
-    'height' => $image->height(),
-    'alt' => $image->alt()->escape()
-  ]);
-}
+/** @var \Kirby\Cms\File|null $image */
+if (isset($image) && !$image) return;
+
+echo Html::img($image->placeholderUri(), [
+  'class' => $class ?? null,
+  'loading' => 'lazy',
+  'data-srcset' => $image->srcset(),
+  'data-sizes' => 'auto',
+  'data-zoomable' => $zoomable ?? null,
+  'width' => $image->width(),
+  'height' => $image->height(),
+  'alt' => $image->alt()->escape()
+]);
