cd54fd1c30aeb5534ec7c4c77cb12918be034038
Johann Schopplich <mail@johannschopplich.com>
2021-09-14 11:12:57 +0200

    refactor: remove fix for 100vh in mobile WebKit

--- src/js/inline-head.js
+++ src/js/inline-head.js
@@ -6,12 +6,6 @@
     document.documentElement.dataset.theme = "dark";
   }
 
-  // Fix opiniated implementation of the `vh` unit in iOS Safari
-  document.documentElement.style.setProperty(
-    "--vh",
-    `${window.innerHeight * 0.01}px`
-  );
-
   // Prevent FOUC by immediately adding `visibility: hidden` styles
   // on elements to animate later on
   const isCrawler = /(gle|ing|ro)bot|crawl|spider/i.test(navigator.userAgent);
