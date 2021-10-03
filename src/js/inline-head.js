f130ba792355f1f290fceaf2387c7c79b6da07d6
Johann Schopplich <mail@johannschopplich.com>
2021-10-03 11:42:13 +0200

    chore: simplify inline head build script

--- src/js/inline-head.js
+++ src/js/inline-head.js
@@ -1,15 +1,13 @@
-(() => {
-  const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
-  const setting = localStorage.getItem("color-schema") || "auto";
+const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
+const setting = localStorage.getItem("color-schema") || "auto";
 
-  if (setting === "dark" || (prefersDark && setting !== "light")) {
-    document.documentElement.dataset.theme = "dark";
-  }
+if (setting === "dark" || (prefersDark && setting !== "light")) {
+  document.documentElement.dataset.theme = "dark";
+}
 
-  // Prevent FOUC by immediately adding `visibility: hidden` styles
-  // on elements to animate later on
-  const isCrawler = /(gle|ing|ro)bot|crawl|spider/i.test(navigator.userAgent);
-  if (!isCrawler) {
-    document.documentElement.dataset.animatable = "true";
-  }
-})();
+// Prevent FOUC by immediately adding `visibility: hidden` styles
+// on elements to animate later on
+const isCrawler = /(gle|ing|ro)bot|crawl|spider/i.test(navigator.userAgent);
+if (!isCrawler) {
+  document.documentElement.dataset.animatable = "true";
+}
