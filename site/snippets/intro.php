adfaa1433938cee8b1e93bf6296260984c785c82
Johann Schopplich <mail@johannschopplich.com>
2021-10-13 11:02:33 +0200

    layout: center intro text

--- site/snippets/intro.php
+++ site/snippets/intro.php
@@ -2,8 +2,8 @@
 /** @var string $title */
 /** @var string|null $props */
 ?>
-<div class="container pt-xl">
-  <h1 class="<?= trim('title is-intro hyphenated ' . ($props ?? ''), ' ') ?>">
+<div class="container is-xl pt-xl">
+  <h1 class="<?= trim('title is-intro md:text-center ' . ($props ?? ''), ' ') ?>">
     <?= $title ?>
   </h1>
   <div class="section-divider" data-animere="GrowSectionDivider"></div>
