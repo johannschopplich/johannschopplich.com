7f9ddf59e5969a185d84433f29b7335bf71ada6b
Johann Schopplich <mail@johannschopplich.com>
2021-10-14 14:01:56 +0200

    refactor: splide init

--- src/js/templates/home.js
+++ src/js/templates/home.js
@@ -2,9 +2,10 @@ export default async () => {
   const { default: Splide } = await import("@splidejs/splide");
   const splide = new Splide(".carousel.is-photography", {
     gap: "var(--space-xs)",
+    fixedHeight: "clamp(35vh, 50vw, 75vh)",
     autoWidth: true,
     focus: "center",
-    lazyLoad: "nearby",
+    lazyLoad: "sequential",
     arrowPath:
       "m23.3 3.3-2.4 2.4 12.7 12.6H0v3.4h33.6L20.9 34.3l2.4 2.4L40 20z",
     pagination: false,
