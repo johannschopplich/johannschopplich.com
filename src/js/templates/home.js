74c1fd27bcfc4f96e4c476e8e77337ee2271d62c
Johann Schopplich <mail@johannschopplich.com>
2021-10-14 12:04:51 +0200

    layout: new carousel arrow

--- src/js/templates/home.js
+++ src/js/templates/home.js
@@ -1,10 +1,12 @@
 export default async () => {
   const { default: Splide } = await import("@splidejs/splide");
   const splide = new Splide(".carousel.is-photography", {
-    gap: "var(--space-s)",
+    gap: "var(--space-xs)",
     autoWidth: true,
     focus: "center",
     lazyLoad: "nearby",
+    arrowPath:
+      "m23.3 3.3-2.4 2.4 12.7 12.6H0v3.4h33.6L20.9 34.3l2.4 2.4L40 20z",
     pagination: false,
   });
 
