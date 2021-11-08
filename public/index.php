365b9326d69f57139b1886acd4cd92713fd4a25b
Johann Schopplich <mail@johannschopplich.com>
2021-11-08 22:31:34 +0100

    refactor: create bootstrap.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,5 @@
 <?php
 
-$base = dirname(__DIR__);
-
-require $base . '/vendor/autoload.php';
-\KirbyExtended\Env::load($base);
-
-$kirby = new \Kirby\Cms\App([
-    'roots' => [
-        'index'    => __DIR__,
-        'base'     => $base,
-        'site'     => $base . '/site',
-        'storage'  => $storage = $base . '/storage',
-        'content'  => $storage . '/content',
-        'accounts' => $storage . '/accounts',
-        'cache'    => $storage . '/cache',
-        'logs'     => $storage . '/logs',
-        'sessions' => $storage . '/sessions'
-    ]
-]);
+require __DIR__ . '/bootstrap.php';
 
 echo $kirby->render();
