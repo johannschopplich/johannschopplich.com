8c37fb199b103089a0b64fd197050e13a9d6206c
Johann Schopplich <mail@johannschopplich.com>
2021-11-09 10:56:39 +0100

    chore: fix ESLint

--- src/modules/registerServiceWorker.ts
+++ src/modules/registerServiceWorker.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useServiceWorker } from "../hooks";
 
 const { register, unregister } = useServiceWorker();
--- src/templates/profile.ts
+++ src/templates/profile.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useBreakpoints } from "../hooks";
 import type { Drauu, Brush } from "drauu";
 
