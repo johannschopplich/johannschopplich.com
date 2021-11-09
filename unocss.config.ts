c96a175e92f32e2fc72e8f2c97034f1b5eadbad9
Johann Schopplich <mail@johannschopplich.com>
2021-11-09 16:09:41 +0100

    refactor: cli options for unocss generator

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,41 +1,41 @@
 import { defineConfig, presetUno } from "unocss";
 import presetIcons from "@unocss/preset-icons";
 
-export function createConfig({ dev = false } = {}) {
-  return defineConfig({
-    envMode: dev ? "dev" : "build",
-    theme: {
-      colors: {
-        primary: "var(--color-primary)",
-        accent: "var(--color-accent)",
-        contrast: {
-          lowest: "var(--color-contrast-lowest)",
-          lower: "var(--color-contrast-lower)",
-          low: "var(--color-contrast-low)",
-          medium: "var(--color-contrast-medium)",
-          high: "var(--color-contrast-high)",
-          higher: "var(--color-contrast-higher)",
-        },
-      },
-      fontFamily: {
-        normal: "var(--font-family-normal)",
-        heading: "var(--font-family-heading)",
-      },
-      lineHeight: {
-        normal: "var(--line-height-normal)",
-        heading: "var(--line-height-heading)",
+export const cliConfig = {
+  outDir: "src/styles",
+  watchPaths: ["site/snippets/**/*", "site/templates/**/*"],
+};
+
+export default defineConfig({
+  theme: {
+    colors: {
+      primary: "var(--color-primary)",
+      accent: "var(--color-accent)",
+      contrast: {
+        lowest: "var(--color-contrast-lowest)",
+        lower: "var(--color-contrast-lower)",
+        low: "var(--color-contrast-low)",
+        medium: "var(--color-contrast-medium)",
+        high: "var(--color-contrast-high)",
+        higher: "var(--color-contrast-higher)",
       },
     },
-    rules: [
-      [/^font-size-(\d+)$/, ([, d]) => ({ "font-size": `var(--text-${d})` })],
-    ],
-    presets: [
-      presetUno(),
-      presetIcons({
-        scale: 1.2,
-      }),
-    ],
-  });
-}
-
-export default createConfig();
+    fontFamily: {
+      normal: "var(--font-family-normal)",
+      heading: "var(--font-family-heading)",
+    },
+    lineHeight: {
+      normal: "var(--line-height-normal)",
+      heading: "var(--line-height-heading)",
+    },
+  },
+  rules: [
+    [/^font-size-(\d+)$/, ([, d]) => ({ "font-size": `var(--text-${d})` })],
+  ],
+  presets: [
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+    }),
+  ],
+});
