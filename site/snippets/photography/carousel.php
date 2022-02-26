6fdaf615076d4bac1a3282cba0f401a4071520e9
Johann Schopplich <mail@johannschopplich.com>
2022-02-26 11:08:04 +0100

    feat: css-only carousel

--- site/snippets/photography/carousel.php
+++ site/snippets/photography/carousel.php
@@ -2,27 +2,26 @@
 /** @var \Kirby\Cms\Page $page */
 /** @var \Kirby\Cms\Files $query */
 ?>
-<div class="carousel splide">
-  <div class="splide__track">
-    <ul class="splide__list">
-      <?php foreach ($query as $image): ?>
-        <?php /** @var \Kirby\Cms\File $image */ ?>
-        <li class="splide__slide">
-          <img
-            class="max-h-full object-contain"
-            src="<?= $image->placeholderUri() ?>"
-            data-splide-lazy="<?= $image->url() ?>"
-            alt="<?= $image->alt()->escape() ?>"
-          >
-        </li>
-      <?php endforeach ?>
+<div
+  class="w-full flex gap-lg snap-x snap-mandatory overflow-x-auto"
+  style="--cell: clamp(35vh, 50vw, 75vh);"
+>
+  <?php foreach ($query as $image): ?>
+    <?php /** @var \Kirby\Cms\File $image */ ?>
+    <div class="snap-always snap-center shrink-0">
+      <?php snippet('helpers/img', [
+        'image' => $image,
+        'class' => 'h-$cell w-auto object-contain'
+      ]) ?>
+    </div>
+  <?php endforeach ?>
 
-      <li class="splide__slide w-[min(65vw,25rem)] flex items-center justify-center border">
-        <a href="<?= $query->first()->parent()->url() ?>" class="action-button">
-          <span class="absolute inset-0" aria-hidden="true"></span>
-          <?= t('photography.morePhotos') ?><span class="i-mdi-arrow-right ml-1"></span>
-        </a>
-      </li>
-    </ul>
+  <div class="snap-always snap-center shrink-0">
+    <div class="relative h-$cell w-[min(65vw,25rem)] flex items-center justify-center border">
+      <a href="<?= $query->first()->parent()->url() ?>" class="action-button">
+        <span class="absolute inset-0" aria-hidden="true"></span>
+        <?= t('photography.morePhotos') ?><span class="i-mdi-arrow-right ml-1"></span>
+      </a>
+    </div>
   </div>
 </div>
