e6cb7ca5460555d9b4080e4d2740d06c9b4811d6
Johann Schopplich <mail@johannschopplich.com>
2022-02-27 18:46:04 +0100

    refactor: simplify project list item margin

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -7,34 +7,32 @@ layout();
 
 <?php snippet('intro', ['title' => $page->text()]) ?>
 
-<div class="content max-w-screen-lg pb-8xl">
-  <div class="border-t pt-xl">
-    <?php foreach ($children = $page->children()->listed() as $project): ?>
-      <?php /** @var \Kirby\Cms\Page $project */ ?>
-      <div class="group relative<?php e(!$project->isLast($children), ' mb-5xl') ?>"<?= attr([
-        'data-animere' => !$project->isFirst($children) ? 'fadeInUpSmall' : null,
-        'data-animere-duration' => '500ms'
-      ], ' ') ?>>
-        <p class="text-contrast-medium" style="word-spacing: 0.25ch;">
-          <?= $project->subtitle()->escape() ?>
-        </p>
+<div class="content max-w-screen-lg pb-8xl space-y-5xl">
+  <?php foreach ($children = $page->children()->listed() as $project): ?>
+    <?php /** @var \Kirby\Cms\Page $project */ ?>
+    <div class="group relative pt-xl border-t"<?= attr([
+      'data-animere' => !$project->isFirst($children) ? 'fadeInUpSmall' : null,
+      'data-animere-duration' => '500ms'
+    ], ' ') ?>>
+      <p class="text-contrast-medium" style="word-spacing: 0.25ch;">
+        <?= $project->subtitle()->escape() ?>
+      </p>
 
-        <h2 class="title text-2xl mb-xl">
-          <a href="<?= $project->url() ?>" class="text-underline leading-tight !text-current">
-            <span class="absolute inset-0" aria-hidden="true"></span>
-            <?= $project->title()->escape() ?>
-          </a>
-        </h2>
+      <h2 class="title text-2xl mb-xl">
+        <a href="<?= $project->url() ?>" class="text-underline leading-tight !text-current">
+          <span class="absolute inset-0" aria-hidden="true"></span>
+          <?= $project->title()->escape() ?>
+        </a>
+      </h2>
 
-        <?php if ($image = $project->thumbnail()->toFile()): ?>
-          <figure class="aspect-ratio-4/3 -mx-lg md:mx-0 md:group-hover:shadow-frame">
-            <?php snippet('helpers/img', [
-              'image' => $image,
-              'class' => 'object-cover h-full'
-            ]) ?>
-          </figure>
-        <?php endif ?>
-      </div>
-    <?php endforeach ?>
-  </div>
+      <?php if ($image = $project->thumbnail()->toFile()): ?>
+        <figure class="aspect-ratio-4/3 -mx-lg md:mx-0 md:group-hover:shadow-frame">
+          <?php snippet('helpers/img', [
+            'image' => $image,
+            'class' => 'object-cover h-full'
+          ]) ?>
+        </figure>
+      <?php endif ?>
+    </div>
+  <?php endforeach ?>
 </div>
