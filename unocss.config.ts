58fa40f98e86607750c1ae5e8b7c5f499a94e0da
Johann Schopplich <mail@johannschopplich.com>
2022-03-01 22:10:15 +0100

    refactor: action button shortcut

--- unocss.config.ts
+++ unocss.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     content: "px-lg md:px-3xl",
     "content-breakout": "-mx-lg md:mx-0",
     "action-button":
-      "p-2 -m-2 text-size-xs font-heading leading-heading font-500 text-center uppercase tracking-[0.125ch]",
+      "p-2 -m-2 text-size-xs font-heading leading-heading font-500 uppercase tracking-[0.125ch]",
     tag: "px-2 py-1 border-1 border-theme-base text-accent text-xs font-heading font-500 leading-heading rounded-full whitespace-nowrap",
   },
   safelist: ["w-full", "aspect-ratio-16/9"],
