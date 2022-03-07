248f508be7b859ffcf22dc876d6f2f214a755b14
Johann Schopplich <mail@johannschopplich.com>
2022-03-07 08:17:49 +0100

    refactor: simplify head script

--- src/head.ts
+++ src/head.ts
@@ -1,17 +1,8 @@
-if (matchMedia("(hover: none)").matches) {
-  document.documentElement.style.setProperty(
-    "--vh",
-    `${window.innerHeight * 0.01}px`
-  );
-}
-
 const root = document.documentElement;
-// const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
-// const setting = localStorage.getItem("color-schema") || "auto";
 
-// if (setting === "dark" || (prefersDark && setting !== "light")) {
-//   root.dataset.theme = "dark";
-// }
+if (matchMedia("(hover: none)").matches) {
+  root.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+}
 
 if (
   !matchMedia("(prefers-reduced-motion: reduce)").matches &&
@@ -19,3 +10,10 @@ if (
 ) {
   root.dataset.animatable = "true";
 }
+
+// const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
+// const setting = localStorage.getItem("color-schema") || "auto";
+
+// if (setting === "dark" || (prefersDark && setting !== "light")) {
+//   root.dataset.theme = "dark";
+// }
