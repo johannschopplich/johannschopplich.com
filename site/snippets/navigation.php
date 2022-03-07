a77e74dcf80022105831d0de0b38d2e8ce6fa4d4
Johann Schopplich <mail@johannschopplich.com>
2022-03-07 11:03:24 +0100

    refactor: simplify navigation

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -4,12 +4,14 @@
 /** @var \Kirby\Cms\Page $page */
 ?>
 <nav class="flex flex-wrap gap-x-lg items-center font-heading">
-  <a href="<?= $site->url() ?>" class="basis-full sm:basis-auto mr-auto font-600" aria-label="<?= t('home') ?>"<?php e($page->isHomePage(), ' aria-current="page"') ?>>
-    <?= $site->title()->escape() ?>
-  </a>
+  <div class="basis-full sm:basis-auto mr-auto">
+    <a href="<?= $site->url() ?>" class="font-600" aria-label="<?= t('home') ?>"<?php e($page->isHomePage(), ' aria-current="page"') ?>>
+      <?= $site->title()->escape() ?>
+    </a>
+  </div>
 
   <?php foreach ($site->children()->listed() as $item): ?>
-    <a href="<?= $item->url() ?>" class="navbar-link flex items-center font-500"<?php e($item->isOpen(), ' aria-current="page"') ?>>
+    <a href="<?= $item->url() ?>" class="navbar-link font-500"<?php e($item->isOpen(), ' aria-current="page"') ?>>
       <?= $item->title() ?>
     </a>
   <?php endforeach ?>
