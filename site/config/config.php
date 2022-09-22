9499fb99dcc94bef43a17728b8fa98975977d7b4
Johann Schopplich <mail@johannschopplich.com>
2022-09-22 17:00:57 +0200

    chore: upgrade dependencies

--- site/config/config.php
+++ site/config/config.php
@@ -43,12 +43,7 @@ return [
         ],
         'sitemap' => [
             'enable' => true
-        ],
-        'vite' => [
-            'server' => [
-                'host' => 'localhost'
-            ]
-        ],
+        ]
     ]
 
 ];
