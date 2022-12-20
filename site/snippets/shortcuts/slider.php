9e13d60e6b5acfef56844c381b861b5acb1a50d5
Johann Schopplich <mail@johannschopplich.com>
2022-12-20 15:41:16 +0100

    fix: slider image max width

--- site/snippets/shortcuts/slider.php
+++ site/snippets/shortcuts/slider.php
@@ -15,25 +15,26 @@ $heightMap = [
   style="--cell: <?= $heightMap[$height ?? 'loose'] ?>"
 >
   <?php foreach ($query as $image): ?>
-    <?php /** @var \Kirby\Cms\File $image */ ?>
-    <?php $settings = $image->gallery()->toObject() ?>
-    <?php $mockup = $settings->mockup()->or('none')->value() ?>
-    <?php $hasMockup = $mockup !== 'none' ?>
-    <?php $tag = ($hasLink = $settings->link()->isNotEmpty()) ? 'a' : 'div' ?>
+    <?php
+      /** @var \Kirby\Cms\File $image */
+      $settings = $image->gallery()->toObject();
+      $mockup = $settings->mockup()->or('none')->value();
+      $tag = ($hasLink = $settings->link()->isNotEmpty()) ? 'a' : 'div';
+      ?>
     <<?= $tag . attr([
       'class' => 'shrink-0 snap-center snap-always first:snap-start',
       'href' => $settings->link()->value(),
       'target' => $hasLink ? '_blank' : null,
       'rel' => $hasLink ? 'noopener' : null
     ], ' ') ?>>
-      <?php if ($hasMockup): ?>
+      <?php if ($mockup !== 'none'): ?>
         <div class="h-$cell bg-$bg p-3xl md:p-5xl relative" style="
           --bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>;
         ">
         <?php if ($mockup === 'mobile'): ?>
           <div class="inset-l-1/2 -ml-1/8 md:bottom-5xl absolute bottom-[calc(1.875rem+6px)] h-[4px] w-1/4 rounded-full bg-zinc-900"></div>
         <?php elseif ($mockup === 'desktop'): ?>
-          <div class="top-[calc(1.875rem+1px)] inset-x-3xl md:top-[calc(3rem+1px)] md:inset-x-5xl absolute flex h-4 items-center gap-1 rounded-t-lg bg-zinc-900 px-2 ">
+          <div class="top-3xl inset-x-3xl md:top-5xl md:inset-x-5xl absolute flex h-4 translate-y-[1px] items-center gap-1 rounded-t-lg bg-zinc-900 px-2">
             <?php foreach (range(1, 3) as $i): ?>
               <div class="bg-contrast-low h-1.5 w-1.5 rounded-full"></div>
             <?php endforeach ?>
@@ -47,11 +48,11 @@ $heightMap = [
           ? 'h-full w-auto rounded-xl border border-zinc-900 object-contain'
           : ($mockup === 'desktop'
             ? 'h-full w-auto rounded-lg border border-zinc-900 object-contain pt-4'
-            : 'h-$cell w-auto object-contain'),
+            : 'h-$cell max-w-screen w-auto object-contain'),
         'zoomable' => $settings->link()->isEmpty() ? true : $zoomable ?? null
       ]) ?>
 
-      <?php if ($hasMockup): ?>
+      <?php if ($mockup !== 'none'): ?>
         </div>
       <?php endif ?>
     </<?= $tag ?>>
