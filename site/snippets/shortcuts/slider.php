9ea518e9d6c0e8b090a67417307c677ef95a9ba2
Johann Schopplich <mail@johannschopplich.com>
2022-12-20 16:10:39 +0100

    refactor: mobile mockup styles

--- site/snippets/shortcuts/slider.php
+++ site/snippets/shortcuts/slider.php
@@ -28,13 +28,13 @@ $heightMap = [
       'rel' => $hasLink ? 'noopener' : null
     ], ' ') ?>>
       <?php if ($mockup !== 'none'): ?>
-        <div class="h-$cell bg-$bg p-3xl md:p-5xl relative" style="
+        <div class="h-$cell bg-$bg p-3xl md:p-5xl relative<?php e($mockup === 'desktop', ' flex flex-col') ?>" style="
           --bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>;
         ">
         <?php if ($mockup === 'mobile'): ?>
-          <div class="inset-l-1/2 -ml-1/8 md:bottom-5xl bottom-3xl absolute h-[2px] w-1/4 translate-y-[-3px] rounded-full bg-zinc-900"></div>
+          <div class="inset-l-1/2 -ml-1/8 md:bottom-5xl bottom-3xl absolute h-[4px] w-1/4 translate-y-[-6px] rounded-full bg-zinc-900"></div>
         <?php elseif ($mockup === 'desktop'): ?>
-          <div class="top-3xl inset-x-3xl md:top-5xl md:inset-x-5xl absolute flex h-4 translate-y-[-1rem] items-center gap-1 rounded-t-lg bg-zinc-900 px-2">
+          <div class="flex h-4 items-center gap-1 rounded-t-lg border-x border-t border-zinc-900 px-2">
             <?php foreach (range(1, 3) as $i): ?>
               <div class="bg-contrast-low h-1.5 w-1.5 rounded-full"></div>
             <?php endforeach ?>
@@ -45,9 +45,9 @@ $heightMap = [
       <?php snippet('shortcuts/img', [
         'file' => $image,
         'class' => $mockup === 'mobile'
-          ? 'h-full w-auto rounded-xl border border-zinc-900 object-contain'
+          ? 'h-full w-auto rounded-xl border-2 border-zinc-900 object-contain'
           : ($mockup === 'desktop'
-            ? 'h-full w-auto rounded-b-lg border border-zinc-900 object-contain'
+            ? 'h-[calc(100%-1rem)] w-auto rounded-b-lg border border-zinc-900 object-contain'
             : 'h-$cell max-w-screen w-auto object-contain'),
         'zoomable' => $settings->link()->isEmpty() ? true : $zoomable ?? null
       ]) ?>
