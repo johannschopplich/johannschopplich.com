971fbe3209fcf6f3e66cad339392c95cd9ace789
Johann Schopplich <mail@johannschopplich.com>
2023-03-08 13:05:13 +0100

    fix: prose styling inside details

--- site/snippets/blocks/details.php
+++ site/snippets/blocks/details.php
@@ -1,5 +1,5 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
-<details>
-  <summary class="prose"><?= $block->summary()->escape() ?></summary>
+<details class="prose">
+  <summary><?= $block->summary()->escape() ?></summary>
   <?= $block->text() ?>
 </details>
