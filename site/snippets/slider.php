3f5699aa9b4f3a55dee742cbd869e29b1b8b864a
Johann Schopplich <mail@johannschopplich.com>
2023-04-10 15:48:35 +0200

    refactor: project mobile mockup design

--- site/snippets/slider.php
+++ site/snippets/slider.php
@@ -43,7 +43,7 @@ $heightMap = [
               style="--bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>"
             >
             <?php if ($isMobile): ?>
-              <div class="inset-l-1/2 -ml-1/8 md:bottom-5xl bottom-3xl absolute h-[2px] w-1/4 translate-y-[-4px] rounded-full bg-zinc-900"></div>
+              <div class="absolute left-1/2 bottom-3xl ml-[-7.5%] h-[2px] w-[15%] translate-y-[-6px] rounded-full bg-zinc-900 md:bottom-5xl"></div>
             <?php elseif ($isDesktop): ?>
               <div class="flex h-4 items-center gap-1 rounded-t-lg border-x border-x-solid border-t border-t-solid border-zinc-900 px-1.5">
                 <?php foreach (range(1, 3) as $i): ?>
