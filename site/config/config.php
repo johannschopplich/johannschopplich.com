67f9e86f6e617185ab614c9c4427fcabc1a3e26d
Johann Schopplich <mail@johannschopplich.com>
2023-06-13 16:01:36 +0200

    chore: enable KQL usage

--- site/config/config.php
+++ site/config/config.php
@@ -83,6 +83,17 @@ return [
         ]
     ],
 
+
+    // Enable basic authentication for the API and thus KQL
+    'api' => [
+        'basicAuth' => true
+    ],
+
+    // Default to token-based authentication
+    'kql' => [
+        'auth' => 'bearer'
+    ],
+
     'headless' => [
         // Disable overwriting of the default routes
         'routes' => false,
