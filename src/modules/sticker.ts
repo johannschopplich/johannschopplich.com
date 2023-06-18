88632a6e298d56e5d7dd62e9204cba8c285c42c6
Johann Schopplich <mail@johannschopplich.com>
2023-06-18 10:53:41 +0200

    refactor: simplify sticker animation code

--- src/modules/sticker.ts
+++ src/modules/sticker.ts
@@ -4,39 +4,13 @@ let animationId: number | undefined;
 let latestMouseEvent: MouseEvent | undefined;
 let startTime: number | undefined;
 
+const duration = 750;
+
 export function install() {
   if (isBelow("md")) return;
 
   const elements = [...document.querySelectorAll<HTMLElement>(".sticker")];
   if (!elements.length) return;
-
-  const rotateLight = (timestamp: number) => {
-    const duration = 750;
-    startTime ??= timestamp;
-
-    // Calculate eased progress
-    const t = Math.min((timestamp - startTime) / duration, 1);
-    const easedT = easeOutCubic(t);
-    const dx = 200 * Math.cos(2 * Math.PI * easedT);
-    const dy =
-      (easedT <= 0.5 ? -1 : 1) * 200 * Math.abs(Math.sin(2 * Math.PI * easedT));
-
-    for (const element of elements) {
-      const light = element.querySelector<HTMLElement>(".sticker-light");
-      if (!light) return;
-
-      light.setAttribute("x", dx.toFixed(2));
-      light.setAttribute("y", dy.toFixed(2));
-    }
-
-    // Request next frame or stop when done
-    if (t < 1) {
-      animationId = requestAnimationFrame(rotateLight);
-    } else {
-      startTime = undefined;
-      animationId = undefined;
-    }
-  };
 
   const handleMouseMove = (event: MouseEvent) => {
     if (animationId) {
@@ -49,14 +23,43 @@ export function install() {
     requestAnimationFrame(() => updateLightPosition(elements));
   };
 
-  window.addEventListener("mousemove", handleMouseMove, {
-    passive: true,
-  });
+  window.addEventListener("mousemove", handleMouseMove, { passive: true });
 
   window.addEventListener("DOMContentLoaded", () => {
     // If not already animating, start
-    animationId ??= requestAnimationFrame(rotateLight);
+    animationId ??= requestAnimationFrame((ts) => rotateLight(ts, elements));
   });
+}
+
+function setLightPosition(light: HTMLElement, dx: number, dy: number) {
+  light.setAttribute("x", dx.toFixed(2));
+  light.setAttribute("y", dy.toFixed(2));
+}
+
+function rotateLight(timestamp: number, elements: HTMLElement[]) {
+  startTime ??= timestamp;
+
+  // Calculate eased progress
+  const t = Math.min((timestamp - startTime) / duration, 1);
+  const easedT = easeOutCubic(t);
+  const dx = 200 * Math.cos(2 * Math.PI * easedT);
+  const dy =
+    (easedT <= 0.5 ? -1 : 1) * 200 * Math.abs(Math.sin(2 * Math.PI * easedT));
+
+  for (const element of elements) {
+    const light = element.querySelector<HTMLElement>(".sticker-light");
+    if (light) {
+      setLightPosition(light, dx, dy);
+    }
+  }
+
+  // Request next frame or stop when done
+  if (t < 1) {
+    animationId = requestAnimationFrame((ts) => rotateLight(ts, elements));
+  } else {
+    startTime = undefined;
+    animationId = undefined;
+  }
 }
 
 function updateLightPosition(elements: HTMLElement[]) {
@@ -66,18 +69,17 @@ function updateLightPosition(elements: HTMLElement[]) {
     const container = element.querySelector<HTMLElement>("svg");
     const light = element.querySelector<HTMLElement>(".sticker-light");
 
-    if (!container || !light) return;
+    if (container && light) {
+      // Calculate the center point of the SVG
+      const { top, left, width, height } = container.getBoundingClientRect();
+      const centerX = left + width / 2;
+      const centerY = top + height / 2;
 
-    // Calculate the center point of the SVG
-    const { top, left, width, height } = container.getBoundingClientRect();
-    const centerX = left + width / 2;
-    const centerY = top + height / 2;
+      const dx = Math.ceil(latestMouseEvent.clientX - centerX);
+      const dy = Math.ceil(latestMouseEvent.clientY - centerY);
 
-    const dx = Math.ceil(latestMouseEvent.clientX - centerX);
-    const dy = Math.ceil(latestMouseEvent.clientY - centerY);
-
-    light.setAttribute("x", dx.toFixed(2));
-    light.setAttribute("y", dy.toFixed(2));
+      setLightPosition(light, dx, dy);
+    }
   }
 
   latestMouseEvent = undefined;
