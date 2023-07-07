b2b776b99fb415c3aadeee5ab63ae7d1227da975
Johann Schopplich <mail@johannschopplich.com>
2023-07-07 16:43:41 +0200

    chore: preload main heading font

--- site/snippets/layouts/default.php
+++ site/snippets/layouts/default.php
@@ -45,6 +45,8 @@
       'assets/fonts/CooperHewitt.css',
       'assets/fonts/IosevkaAile.css'
     ]) ?>
+  <?php else: ?>
+    <link rel="preload" href="<?= vite()->file('../public/assets/fonts/woff2/CooperHewitt-Heavy.woff2') ?>" as="font" type="font/woff2" crossorigin>
   <?php endif ?>
 
   <?= vite()->js('main.ts') ?>
