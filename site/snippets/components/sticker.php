018a5301b3ff699c2451b526bdc69ebe8b2f4f01
Johann Schopplich <mail@johannschopplich.com>
2023-09-12 18:19:13 +0200

    fix: sticker layout position

--- site/snippets/components/sticker.php
+++ site/snippets/components/sticker.php
@@ -2,5 +2,5 @@
 /** @var string|null $svg */
 ?>
 <div class="sticker inline-block h-[0.875em] w-[0.875em] translate-y-[0.05em] select-none">
-  <span class="[&>svg]:w-full [&>svg]:h-full"><?= $svg ?></span>
+  <span class="[&>svg]:w-auto [&>svg]:h-full"><?= $svg ?></span>
 </div>
