01cc0bf490ca25c2ec43a550454d21917473d784
Johann Schopplich <mail@johannschopplich.com>
2023-09-13 10:04:36 +0200

    fix: wait for current fetch promise to resolve

--- src/modules/sticker-svg.ts
+++ src/modules/sticker-svg.ts
@@ -1,6 +1,7 @@
 import { getParsedSiteData } from "../utils";
 
 const cache = new Map<string, string>();
+let isFetching = false;
 
 export function install(): void {
   const elements = [
@@ -10,21 +11,30 @@ export function install(): void {
 
   // Handle clicks on the sticker
   document.addEventListener("click", async (event) => {
+    if (isFetching) return;
+
     const target = event.target as HTMLElement;
     const container = target.closest<HTMLElement>('[data-sticker="svg"]');
+    if (!container) return;
 
-    if (container) {
-      const svg = container.querySelector<SVGElement>("svg");
-      const newSvgString = await fetchRandomSvg();
-      const parser = new DOMParser();
-      const newSvgDoc = parser.parseFromString(newSvgString, "image/svg+xml");
-      const newSvg = newSvgDoc.querySelector("svg");
-
-      // Replace the SVG with a random one
-      if (svg && newSvg) {
-        container.replaceChild(newSvg, svg);
-      }
+    isFetching = true;
+    const svg = container.querySelector<SVGElement>("svg");
+    const newSvgString = await fetchRandomSvg();
+    if (!newSvgString) {
+      isFetching = false;
+      return;
     }
+
+    const parser = new DOMParser();
+    const newSvgDoc = parser.parseFromString(newSvgString, "image/svg+xml");
+    const newSvg = newSvgDoc.querySelector("svg");
+
+    // Replace the SVG with a random one
+    if (svg && newSvg) {
+      container.replaceChild(newSvg, svg);
+    }
+
+    isFetching = false;
   });
 }
 
@@ -37,13 +47,17 @@ async function fetchRandomSvg() {
     return cache.get(randomIcon)!;
   }
 
-  const response = await fetch(`/assets/img/icons/${randomIcon}`);
-  if (!response.ok) {
-    console.error(`Failed to fetch SVG: ${response.status}`);
+  try {
+    const response = await fetch(`/assets/img/icons/${randomIcon}`);
+    if (!response.ok) {
+      console.error(`Failed to fetch SVG: ${response.status}`);
+      return;
+    }
+
+    const svg = await response.text();
+    cache.set(randomIcon, svg);
+    return svg;
+  } catch (error) {
+    console.error(`Failed to fetch SVG: ${error}`);
   }
-
-  const svg = await response.text();
-
-  cache.set(randomIcon, svg);
-  return svg;
 }
