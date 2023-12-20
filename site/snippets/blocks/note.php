96767c8659c8a3b3ae822d50830dfe9fdff284a3
Johann Schopplich <mail@johannschopplich.com>
2023-12-20 07:48:07 +0100

    chore: update note styling

--- site/snippets/blocks/note.php
+++ site/snippets/blocks/note.php
@@ -1,4 +1,4 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
-<div class="bg-primary-500 text-white py-[1.25rem] px-[1.5rem]">
+<div class="border border-solid border-primary-500 text-size-[0.875rem] py-[1rem] px-[1.25rem] shadow-frame">
   <?= $block->text()->permalinksToUrls() ?>
 </div>
