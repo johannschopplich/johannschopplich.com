c884f375c0024bf1b21fadae37876918de33f04a
Johann Schopplich <mail@johannschopplich.com>
2024-02-22 16:37:45 +0100

    fix: init Algolia container once

--- src/modules/algolia.ts
+++ src/modules/algolia.ts
@@ -11,20 +11,24 @@ export async function install() {
 
   document
     .querySelector<HTMLButtonElement>(".DocSearch-Button")
-    ?.addEventListener("click", () => {
-      DocSearch({
-        ...algolia,
-        container,
-        indexName: `johannschopplich-${lang}`,
-        placeholder: {
-          en: "Search Site",
-          de: "Seite durchsuchen",
-        }[lang],
-        translations: locales[lang],
-        // insights: true,
-      });
+    ?.addEventListener(
+      "click",
+      () => {
+        DocSearch({
+          ...algolia,
+          container,
+          indexName: `johannschopplich-${lang}`,
+          placeholder: {
+            en: "Search Site",
+            de: "Seite durchsuchen",
+          }[lang],
+          translations: locales[lang],
+          // insights: true,
+        });
 
-      // Simulate click on the button to open the search modal
-      document.querySelector<HTMLElement>(".DocSearch-Button")?.click();
-    });
+        // Simulate click on the button to open the search modal
+        document.querySelector<HTMLElement>(".DocSearch-Button")?.click();
+      },
+      { once: true },
+    );
 }
