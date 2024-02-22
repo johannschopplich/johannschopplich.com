54bd02fd7fe2c53ebb9ce9337ff47c3114683e3d
Johann Schopplich <mail@johannschopplich.com>
2024-02-22 16:27:54 +0100

    perf: remove slider image fade-in animation

--- site/snippets/components/slider.php
+++ site/snippets/components/slider.php
@@ -32,51 +32,46 @@ $heightMap = [
         'href' => $hasLink ? $settings->link()->value() : null,
         'target' => $hasLink ? '_blank' : null
       ], ' ') ?>>
-        <div
-          data-slide-content
-          style="--animate-duration: 250ms; --animate-delay: <?= $image->indexOf($query) * 50 + 50 . 'ms' ?>"
-        >
-          <?php if ($mockup !== 'none'): ?>
-            <div
-              class="<?= implode(' ', [
-                'h-$cell bg-$bg relative',
-                ($isMobile || $isDocument) ? 'px-5xl py-3xl md:px-8xl md:py-5xl xl:px-[9rem]' : 'p-3xl md:p-5xl',
-                $isDesktop ? 'flex flex-col' : ''
-              ]) ?>"
-              style="--bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>"
-            >
-            <?php if ($isMobile): ?>
-              <div class="absolute left-1/2 bottom-3xl h-[1px] w-[15%] bg-stone-900 ml-[-7.5%] rounded-full translate-y-[-4px] md:bottom-5xl md:h-[2px] md:translate-y-[-6px]"></div>
-            <?php elseif ($isDesktop): ?>
-              <div class="flex h-4 items-center gap-1 border-x border-x-solid border-t border-t-solid border-stone-900 rounded-t-lg px-1.5">
-                <?php foreach (range(1, 3) as $i): ?>
-                  <div class="h-1.5 w-1.5 border border-solid border-stone-900 rounded-full"></div>
-                <?php endforeach ?>
-              </div>
-            <?php endif ?>
-          <?php endif ?>
-
-          <img
+        <?php if ($mockup !== 'none'): ?>
+          <div
             class="<?= implode(' ', [
-              ($isDocument || $isMobile) ? 'h-full w-auto border border-solid border-stone-900 object-contain' : '',
-              $isMobile ? 'rounded-xl' : '',
-              $isDesktop ? 'h-[calc(100%-1rem)] w-auto rounded-b-lg border border-solid border-stone-900 object-contain' : '',
-              ($mockup === 'none') ? 'h-$cell max-w-screen w-auto object-contain' : ''
+              'h-$cell bg-$bg relative',
+              ($isMobile || $isDocument) ? 'px-5xl py-3xl md:px-8xl md:py-5xl xl:px-[9rem]' : 'p-3xl md:p-5xl',
+              $isDesktop ? 'flex flex-col' : ''
             ]) ?>"
-            loading="lazy"
-            src="<?= $image->thumbhashUri() ?>"
-            data-srcset="<?= $image->srcset() ?>"
-            data-sizes="auto"
-            width="<?= $image->width() ?>"
-            height="<?= $image->height() ?>"
-            alt="<?= $image->alt()->or('')->escape() ?>"
-            data-slide-image
+            style="--bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>"
           >
-
-          <?php if ($mockup !== 'none'): ?>
+          <?php if ($isMobile): ?>
+            <div class="absolute left-1/2 bottom-3xl h-[1px] w-[15%] bg-stone-900 ml-[-7.5%] rounded-full translate-y-[-4px] md:bottom-5xl md:h-[2px] md:translate-y-[-6px]"></div>
+          <?php elseif ($isDesktop): ?>
+            <div class="flex h-4 items-center gap-1 border-x border-x-solid border-t border-t-solid border-stone-900 rounded-t-lg px-1.5">
+              <?php foreach (range(1, 3) as $i): ?>
+                <div class="h-1.5 w-1.5 border border-solid border-stone-900 rounded-full"></div>
+              <?php endforeach ?>
             </div>
           <?php endif ?>
-        </div>
+        <?php endif ?>
+
+        <img
+          class="<?= implode(' ', [
+            ($isDocument || $isMobile) ? 'h-full w-auto border border-solid border-stone-900 object-contain' : '',
+            $isMobile ? 'rounded-xl' : '',
+            $isDesktop ? 'h-[calc(100%-1rem)] w-auto rounded-b-lg border border-solid border-stone-900 object-contain' : '',
+            ($mockup === 'none') ? 'h-$cell max-w-screen w-auto object-contain' : ''
+          ]) ?>"
+          loading="lazy"
+          src="<?= $image->thumbhashUri() ?>"
+          data-srcset="<?= $image->srcset() ?>"
+          data-sizes="auto"
+          width="<?= $image->width() ?>"
+          height="<?= $image->height() ?>"
+          alt="<?= $image->alt()->or('')->escape() ?>"
+          data-slide-image
+        >
+
+        <?php if ($mockup !== 'none'): ?>
+          </div>
+        <?php endif ?>
       </<?= $tag ?>>
     <?php endforeach ?>
 
--- src/modules/slider.ts
+++ src/modules/slider.ts
@@ -1,4 +1,3 @@
-import { animate } from "animere";
 import { lazyLoad } from "unlazy";
 import { getRootFontSize } from "../utils";
 
@@ -12,61 +11,30 @@ export async function install() {
   // Use scroll snap slider for mobile devices
   if (isTouchscreen) {
     lazyLoad("[data-slider] [data-slide-image]");
-  } else {
-    const { default: Swiper } = await import("swiper");
-
-    for (const element of elements) {
-      // Remove classes that are interfering with Swiper.js
-      element.firstElementChild?.setAttribute("class", "swiper-wrapper");
-
-      // eslint-disable-next-line no-new
-      new Swiper(element, {
-        slidesPerView: "auto",
-        spaceBetween: 0.75 * fontSize,
-        // centeredSlides: true,
-        // centeredSlidesBounds: true,
-        grabCursor: true,
-        longSwipesRatio: 0.25,
-        on: {
-          afterInit() {
-            const images =
-              element.querySelectorAll<HTMLImageElement>("[data-slide-image]");
-            lazyLoad(images);
-          },
-        },
-      });
-    }
+    return;
   }
 
-  // Skip animations if reduced motion is enabled
-  if (!("animatable" in document.documentElement.dataset)) return;
+  const { default: Swiper } = await import("swiper");
 
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (!entry.isIntersecting) return;
-
-      for (const slide of entry.target.querySelectorAll<HTMLElement>(
-        "[data-slide-content]",
-      )) {
-        if (slide.classList.contains("invisible")) {
-          slide.classList.remove("invisible");
-          animate(slide, "fadeInLeft", "animate__");
-        }
-      }
-    },
-    { threshold: 0.25, rootMargin: "50px" },
-  );
-
-  // If the slider is not initially in viewport, hide slides and start observer
   for (const element of elements) {
-    const rect = element.getBoundingClientRect();
-    if (rect.top >= window.innerHeight || rect.bottom <= 0) {
-      for (const slide of element.querySelectorAll<HTMLElement>(
-        "[data-slide-content]",
-      )) {
-        slide.classList.add("invisible");
-      }
-      observer.observe(element);
-    }
+    // Remove classes that are interfering with Swiper.js
+    element.firstElementChild?.setAttribute("class", "swiper-wrapper");
+
+    // eslint-disable-next-line no-new
+    new Swiper(element, {
+      slidesPerView: "auto",
+      spaceBetween: 0.75 * fontSize,
+      // centeredSlides: true,
+      // centeredSlidesBounds: true,
+      grabCursor: true,
+      longSwipesRatio: 0.25,
+      on: {
+        afterInit() {
+          const images =
+            element.querySelectorAll<HTMLImageElement>("[data-slide-image]");
+          lazyLoad(images);
+        },
+      },
+    });
   }
 }
