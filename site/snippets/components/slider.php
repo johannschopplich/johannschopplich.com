1186128b810073fd2f5d4e50f80c3259f0e9d569
Johann Schopplich <mail@johannschopplich.com>
2024-10-28 18:04:02 +0100

    chore: update mobile mockup padding

--- site/snippets/components/slider.php
+++ site/snippets/components/slider.php
@@ -39,8 +39,7 @@ $heightMap = [
         class="<?= trim(implode(' ', [
                   'group-hover:ring-1 group-hover:ring-theme-base',
                   $mockup !== 'none' ? 'relative h-$cell bg-$bg' : '',
-                  $isDocument ? 'px-5xl py-xl md:px-8xl md:py-5xl xl:px-[9rem]' : '',
-                  $isMobile ? 'px-[4.5rem] py-3xl md:px-8xl md:py-5xl xl:px-[9rem]' : '',
+                  ($isDocument || $isMobile) ? 'px-[4.5rem] py-xl md:px-8xl md:py-5xl xl:px-[9rem]' : '',
                   $isDesktop ? 'flex flex-col p-3xl md:p-5xl' : ''
                 ]), ' ') ?>"
         style="--bg: <?= $settings->bgColor()->or('var(--du-color-contrast-lower)') ?>">
