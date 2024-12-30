854226fbd1294c02ead463183a1df06515ce8291
Johann Schopplich <mail@johannschopplich.com>
2024-12-30 21:59:33 +0100

    refactor: RSS feed template

--- site/snippets/feed/rss.php
+++ site/snippets/feed/rss.php
@@ -3,15 +3,14 @@
 use Kirby\Toolkit\Xml;
 
 echo '<?xml version="1.0" encoding="utf-8"?>';
-?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+?><rss version="2.0">
   <channel>
     <title><?= Xml::encode($title) ?></title>
     <link><?= Xml::encode($link) ?></link>
     <lastBuildDate><?= $modified ?></lastBuildDate>
-    <atom:link href="<?= Xml::encode($feedurl) ?>" rel="self" type="application/rss+xml" />
     <?php if ($description && is_string($description) && strlen(trim($description)) > 0): ?>
       <description><?= Xml::encode($description) ?></description>
-    <?php endif; ?>
+    <?php endif ?>
     <?php foreach ($items as $item): ?>
       <item>
         <title><?= Xml::encode($item->{$titlefield}()) ?></title>
@@ -22,6 +21,6 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
           <![CDATA[<?= $item->{$textfield}()->toBlocks() ?>]]>
         </description>
       </item>
-    <?php endforeach; ?>
+    <?php endforeach ?>
   </channel>
 </rss>
