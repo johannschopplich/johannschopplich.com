b8695f6ba2b3175306e1380b782d98d9159581fa
Johann Schopplich <mail@johannschopplich.com>
2025-01-04 12:17:23 +0100

    chore: update bg color

--- site/templates/project.php
+++ site/templates/project.php
@@ -16,7 +16,7 @@ $isMasonry = $page->galleryType()->value() === 'masonry';
   </div>
 
   <div class="content-l pb-5xl">
-    <div class="flex gap-1 items-center mt-1 after:content-empty after:flex-1 after:h-[1px] after:bg-current un-dark:after:bg-contrast-lower">
+    <div class="flex gap-1 items-center mt-1 after:content-empty after:flex-1 after:h-[1px] after:bg-contrast-low un-dark:after:bg-contrast-lower">
       <p class="shrink-0 text-sm font-500">
         <?= $page->subtitle()->escape() ?>
       </p>
