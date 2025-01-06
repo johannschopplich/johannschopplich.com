ec53f4998f97253aa9a9c2afcf863ab72b853f48
Johann Schopplich <mail@johannschopplich.com>
2025-01-06 07:16:41 +0100

    chore: update nav underline

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -16,8 +16,7 @@
       class="flex items-center md:before:content-empty md:before:w-[0.5em] md:before:h-[0.5em] md:before:rounded-full md:before:bg-contrast-lower md:before:mr-1 lg:before:mr-2
              md:hover:text-current md:hover:before:bg-primary md:focus:before:bg-primary
              md:[&[aria-current]]:before:bg-current
-             max-md:underline max-md:decoration-transparent max-md:decoration-size-[var(--un-decoration-thickness)] max-md:underline-offset-[var(--un-decoration-offset)]
-             max-md:hover:decoration-primary max-md:[&[aria-current]]:decoration-primary
+             max-md:hover:underline-default max-md:[&[aria-current]]:underline-default
              font-500"
       <?php e($item->isOpen(), ' aria-current="page"') ?>>
       <?= $item->title() ?>
--- uno.config.ts
+++ uno.config.ts
@@ -109,16 +109,15 @@ export default defineConfig<Theme>({
       "content-l": "pl-lg md:pl-[max(4vw,1.875rem)]",
       "underline-default":
         "underline decoration-current decoration-size-[var(--un-decoration-thickness)] underline-offset-[var(--un-decoration-offset)]",
-      icon: "w-[1.25em] h-[1.25em] [&>svg]:w-full [&>svg]:h-full",
-      "cta-button":
-        "inline-flex items-center text-size-xs leading-[1.05] font-heading font-500 -m-2 p-2 uppercase tracking-[0.125ch]",
-      tag: "text-size-xs leading-[1.05] font-heading font-500 whitespace-nowrap border-1 border-solid border-theme-base rounded-full px-2 py-1 un-dark:border-contrast-low",
       headline:
         "text-contrast-higher text-size-2xl leading-[1.15] font-heading font-900 md:text-size-[calc(var(--un-text-4xl)+0.5vw)] md:leading-[1.05]",
+      "section-divider": "h-[min(20svh,8rem)]",
+      icon: "w-[1.25em] h-[1.25em] [&>svg]:w-full [&>svg]:h-full",
+      tag: "text-size-xs leading-[1.05] font-heading font-500 whitespace-nowrap border-1 border-solid border-theme-base rounded-full px-2 py-1 un-dark:border-contrast-low",
+      "cta-button":
+        "inline-flex items-center text-size-xs leading-[1.05] font-heading font-500 -m-2 p-2 uppercase tracking-[0.125ch]",
       "masonry-grid":
         "grid grid-cols-[repeat(auto-fit,minmax(min(var(--masonry-column-max-width,25rem),100%),1fr))] justify-center children:self-start", // grid-rows-[masonry]
-      "filter-blur": "[filter:url(#blur-and-scale)]",
-      "section-divider": "h-[min(20svh,8rem)]",
     },
   ],
   safelist: [
