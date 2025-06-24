49fe949eb60ca1f07d74e2a7fb168e689fa4c58c
Johann Schopplich <mail@johannschopplich.com>
2025-06-24 07:14:03 +0200

    chore: update thumbs quality and srcsets in config

--- site/config/config.php
+++ site/config/config.php
@@ -37,12 +37,23 @@ return [
 
     'thumbs' => [
         'format' => 'webp',
-        'quality' => 80,
+        'quality' => 85,
         'presets' => [
-            'default' => ['format' => 'webp', 'quality' => 80],
+            'default' => ['format' => 'webp', 'quality' => 85],
         ],
         'srcsets' => [
-            'default' => [360, 720, 1024, 1280, 1536]
+            'default' => [
+                390, // iPhone 12/13/14
+                430, // iPhone 15 Pro Max
+                780, // iPhone 12/13/14 @2x
+                860, // iPhone 15 Pro Max @2x
+                1170, // iPhone 12/13/14 @3x
+                1366, // Common laptop resolution
+                1440, // MacBook, many modern laptops
+                1920, // Full HD
+                2580, // iPhone 15 Pro Max @3x
+                2880, // MacBook @2x
+            ]
         ]
     ],
 
