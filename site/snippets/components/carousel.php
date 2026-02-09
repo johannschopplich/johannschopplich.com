5fdbe43bd0950649a396cdd1c54790d6dde44087
Johann Schopplich <mail@johannschopplich.com>
2026-02-09 14:44:04 +0100

    refactor(carousel): simplify code style

--- site/snippets/components/carousel.php
+++ site/snippets/components/carousel.php
@@ -24,7 +24,7 @@ $selectedHeight = match ($height ?? null) {
   <div class="flex gap-xs cursor-grab active:cursor-grabbing" aria-live="polite">
     <?php foreach ($query->values() as $index => $image): ?>
       <?php
-      /** @var \Kirby\Cms\File $image */
+      /** @var \Kirby\Content\Content */
       $settings = $image->gallery()->toObject();
       $mockup = $settings->mockup()->or('none')->value();
       $isDocument = $mockup === 'document';
@@ -41,11 +41,11 @@ $selectedHeight = match ($height ?? null) {
       >
         <div
           class="<?= trim(implode(' ', [
-                    'overflow-hidden',
-                    $mockup !== 'none' ? 'relative bg-$cell-bg' : '',
-                    ($isDocument || $isMobile) ? 'h-$cell-h px-[4.5rem] py-xl md:px-8xl md:py-5xl xl:px-[9rem]' : '',
-                    $isDesktop ? 'flex flex-col items-center justify-center h-$cell-h p-3xl md:p-5xl w-[calc(100vw-2.25rem)] md:w-auto' : ''
-                  ]), ' ') ?>"
+            'overflow-hidden',
+            $mockup !== 'none' ? 'relative bg-$cell-bg' : '',
+            ($isDocument || $isMobile) ? 'h-$cell-h px-[4.5rem] py-xl md:px-8xl md:py-5xl xl:px-[9rem]' : '',
+            $isDesktop ? 'flex flex-col items-center justify-center h-$cell-h p-3xl md:p-5xl w-[calc(100vw-2.25rem)] md:w-auto' : ''
+          ]), ' ') ?>"
           style="--cell-bg: <?= $bgHex ?? 'var(--un-color-contrast-lower)' ?>"
         >
           <?php if ($isDesktop): ?>
@@ -56,39 +56,33 @@ $selectedHeight = match ($height ?? null) {
             </div>
           <?php endif ?>
 
-          <?php
-          $aspectRatio = 'aspect-ratio: ' . $image->width() . '/' . $image->height();
-          $imgStyles = $mockup === 'none'
-            ? implode('; ', [
-              '--img-h: min(calc(100vw * ' . $image->height() . ' / ' . $image->width() . '), var(--cell-h))',
-              $aspectRatio
-            ])
-            : $aspectRatio;
-          ?>
           <?php if ($isDocument):
-            $borderStyle = '';
-            if ($hasBgColor) {
-              $bgColor = new Hex($bgHex);
-              $borderStyle = '--cell-border: ' . ($bgColor->isDark() ? $bgColor->lighten(20) : $bgColor->darken(20));
-            }
+            $bgColor = $hasBgColor ? new Hex($bgHex) : null;
+            $borderColor = $bgColor ? ($bgColor->isDark() ? $bgColor->lighten(20) : $bgColor->darken(20)) : null;
           ?>
-            <div class="h-full w-fit p-2 border border-dashed <?= $hasBgColor ? 'border-$cell-border' : 'border-contrast-low' ?> md:p-3"<?php if ($borderStyle): ?> style="<?= $borderStyle ?>"<?php endif ?>>
+            <div
+              class="h-full w-fit p-2 border border-dashed border-$cell-border md:p-3"
+              style="--cell-border: <?= $borderColor ?? 'var(--un-color-contrast-low)' ?>"
+            >
           <?php endif ?>
 
           <img
             class="<?= trim(implode(' ', [
-                      'pointer-events-none select-none',
-                      $mockup === 'none' ? 'w-auto max-w-[100vw] h-$img-h' : '',
-                      $isDocument ? 'w-auto h-full object-cover shadow-[0_1px_3px_0_oklch(0_0_0/0.1),_0_4px_12px_-2px_oklch(0_0_0/0.08)]' : '',
-                      $isMobile ? 'w-auto h-full object-cover rounded-2xl shadow-[0_0_0_1px_oklch(1_0_0/0.1),_0_0_0_1px_oklch(0_0_0/0.1),_0_8px_24px_-4px_oklch(0_0_0/0.12),_0_2px_6px_-1px_oklch(0_0_0/0.1)]' : '',
-                      $isDesktop ? 'w-full h-auto md:w-auto md:h-[calc(100%-1rem)] border border-solid border-stone-900 rounded-b-lg' : ''
-                    ]), ' ') ?>"
+              'pointer-events-none select-none',
+              $mockup === 'none' ? 'w-auto max-w-[100vw] h-$img-h' : '',
+              $isDocument ? 'w-auto h-full object-cover shadow-[0_1px_3px_0_oklch(0_0_0/0.1),_0_4px_12px_-2px_oklch(0_0_0/0.08)]' : '',
+              $isMobile ? 'w-auto h-full object-cover rounded-2xl shadow-[0_0_0_1px_oklch(1_0_0/0.1),_0_0_0_1px_oklch(0_0_0/0.1),_0_8px_24px_-4px_oklch(0_0_0/0.12),_0_2px_6px_-1px_oklch(0_0_0/0.1)]' : '',
+              $isDesktop ? 'w-full h-auto md:w-auto md:h-[calc(100%-1rem)] border border-solid border-stone-900 rounded-b-lg' : ''
+            ]), ' ') ?>"
             src="<?= $image->thumbhashUri() ?>"
             data-srcset="<?= $image->srcset() ?>"
             data-sizes="auto"
             width="<?= $image->width() ?>"
             height="<?= $image->height() ?>"
-            style="<?= $imgStyles ?>"
+            style="<?= trim(implode('; ', [
+              $mockup === 'none' ? '--img-h: min(calc(100vw * ' . $image->height() . ' / ' . $image->width() . '), var(--cell-h))' : '',
+              'aspect-ratio: ' . $image->width() . '/' . $image->height()
+            ]), ' ') ?>"
             alt="<?= $image->alt()->or('')->escape() ?>"
           >
 
