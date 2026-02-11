462b94f4ed4089153b3b3d0da49adf743f22a978
Johann Schopplich <mail@johannschopplich.com>
2026-02-11 07:36:00 +0100

    chore: update logo height and sizing

--- site/snippets/partials/navigation.php
+++ site/snippets/partials/navigation.php
@@ -8,14 +8,14 @@
   <a
     href="<?= $site->url() ?>"
     class="
-      inline-flex items-center bg-theme-background halftone-bg pl-0.5 pr-0.75 pb-0.75 pt-1
+      inline-flex items-center bg-theme-background halftone-bg pl-0.5 pr-1 py-0.5
       [--un-dithered-from:var(--un-color-contrast-soft)] hover:[--un-dithered-from:currentcolor]
       aria-[current]:[--un-dithered-from:var(--un-color-contrast-high)]
     "
     <?php e($page->isHomePage(), ' aria-current="page"') ?>
     aria-label="<?= $site->title()->escape() ?>"
   >
-    <svg class="size-[1.1em]" viewBox="0 0 256 256" fill="currentColor" style="overflow: visible; stroke: var(--un-dithered-to, var(--un-color-background)); stroke-width: 64; paint-order: stroke fill;" aria-hidden="true">
+    <svg class="size-[1.25em]" viewBox="0 0 256 256" fill="currentColor" style="overflow: visible; stroke: var(--un-dithered-to, var(--un-color-background)); stroke-width: 64; paint-order: stroke fill;" aria-hidden="true">
       <path d="M95.302 207.282c0 30.72-16.446 48.717-48.407 48.717-4.965 0-11.171-.31-14.895-1.551v-30.72c2.172.31 5.586.931 8.378.931 11.171 0 16.446-5.275 16.446-17.687V62.991h38.477l.001 144.291ZM143.604 160.426c3.103 9.93 11.792 15.205 23.273 15.205 11.791 0 19.549-4.654 19.549-13.653 0-6.206-4.344-9.309-13.963-11.791l-27.927-6.206c-21.1-4.965-35.995-15.825-35.995-39.098 0-26.996 24.204-44.994 55.234-44.994 35.064 0 54.303 16.756 58.957 42.822h-36.616c-2.482-7.757-8.999-13.653-21.721-13.653-9.309 0-18.308 4.344-18.308 13.033 0 5.585 3.724 8.688 12.412 10.86l28.548 6.516c24.204 5.896 36.926 19.239 36.926 40.339 0 28.237-24.514 44.994-56.785 44.994-33.823 0-57.716-16.136-61.75-44.373l38.166-.001ZM222.731 0H56.824v38.477h165.907V0Z" />
     </svg>
   </a>
