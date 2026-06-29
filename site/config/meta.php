09fb1a86fbc3b2855b5743d6f51ae370cc8e0e0c
Johann Schopplich <johann@schopplich.com>
2026-06-29 19:24:44 +0200

    refactor(meta): use full closure for meta defaults

--- site/config/meta.php
+++ site/config/meta.php
@@ -4,23 +4,25 @@ use Kirby\Cms\App;
 use Kirby\Cms\Page;
 use Kirby\Cms\Site;
 
-return fn (App $kirby, Site $site, Page $page) => [
-    'jsonld' => [
-        'Person' => $site->person(),
-        'WebSite' => [
-            '@id' => $site->webSiteId(),
-            'name' => $site->title()->value(),
-            'description' => $site->description()->value(),
-            'url' => $site->url(),
-            'inLanguage' => $kirby->languageCode(),
-            'author' => $site->personReference()
-        ],
-        'WebPage' => [
-            '@id' => $page->webPageId(),
-            'url' => $page->url(),
-            'name' => $page->customTitle()->or($page->title())->value(),
-            'inLanguage' => $kirby->languageCode(),
-            'isPartOf' => $site->webSiteReference()
+return function (App $kirby, Site $site, Page $page): array {
+    return [
+        'jsonld' => [
+            'Person' => $site->person(),
+            'WebSite' => [
+                '@id' => $site->webSiteId(),
+                'name' => $site->title()->value(),
+                'description' => $site->description()->value(),
+                'url' => $site->url(),
+                'inLanguage' => $kirby->languageCode(),
+                'author' => $site->personReference()
+            ],
+            'WebPage' => [
+                '@id' => $page->webPageId(),
+                'url' => $page->url(),
+                'name' => $page->customTitle()->or($page->title())->value(),
+                'inLanguage' => $kirby->languageCode(),
+                'isPartOf' => $site->webSiteReference()
+            ]
         ]
-    ]
-];
+    ];
+};
